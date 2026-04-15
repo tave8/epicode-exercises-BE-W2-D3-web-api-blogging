c8314319a99a933daa8fe025dc9030c861e80100
Giuseppe Tavella <giuseppetavella8@gmail.com>
2026-04-15 15:07:58 +0200

    Initial commit

---
+++ src/main/java/giuseppetavella/web_api_blogging/WebApiBloggingApplication.java
@@ -0,0 +1,13 @@
+package giuseppetavella.web_api_blogging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApiBloggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebApiBloggingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/giuseppetavella/web_api_blogging/WebApiBloggingApplicationTests.java
@@ -0,0 +1,13 @@
+package giuseppetavella.web_api_blogging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApiBloggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
